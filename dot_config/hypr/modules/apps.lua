2fecd52609a9950bb08f2ad3cf6140507485bb1e
Ony <ony.rakotonirina@fluentech-group.com>
2026-09-25 18:28:52 +0300

    cleanup

--- dot_config/hypr/modules/apps.lua
+++
@@ -1,9 +0,0 @@
-return {
-	mod = "SUPER",
-	terminal = "ghostty",
-	fileManager = "nautilus",
-	menu = "vicinae toggle",
-	browser = "google-chrome",
-	clipboard = "vicinae vicinae://launch/clipboard/history",
-	emoji = "vicinae vicinae://launch/core/search-emojis",
-}
