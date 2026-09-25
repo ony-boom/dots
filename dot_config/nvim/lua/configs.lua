d4b55ca18966c8f778da6201ce9d131055eba6c2
Ony <ony.rakotonirina@fluentech-group.com>
2026-09-25 19:07:30 +0300

    better kitty conf

--- dot_config/nvim/lua/configs.lua
+++ dot_config/nvim/lua/configs.lua
@@ -4,7 +4,7 @@ vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function() vim.hl.hl_op() end,
+  callback = function() (vim.hl.hl_op or vim.hl.on_yank)() end,
 })
 
 vim.diagnostic.config {
